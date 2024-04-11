87a92c39662511d9eba087f130ed8bc20d8685e4
Bayron Guerrero Ruiz <162919765+BayronGR@users.noreply.github.com>
2024-04-11 15:09:51 -0600

    Iniciación del registro y monitoreo en ejecución

--- ScriptProcesos3.sh
+++ ScriptProcesos3.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-# Se verifica que no el atgumento proporcionados no es igual a 1
+# Se verifica que el argumento proporcionados no es igual a 1
 if [ $# -ne 1 ]; then
   echo "Uso: $0 <nombre_del_ejecutable>"
   exit 1
 fi
 
-# Nombre del ejecutable a monitorear
+# Nombre del ejecutable que se quiere monitorear
 ejecutable="$1"
 
 # Duración del monitoreo
-duracion_monitoreo=60 
+duracion_monitoreo=30 
 
 # Nombre del archivo de registro
 log_file="monitoring.log"
 
-# Inicio de ejecucion
+# Inicio de ejecución
 start_time=$(awk '{print int($1)}' /proc/uptime)
+
+# Iniciación del archivo de registro 
+if [ ! -e "$log_file" ]; then
+  echo "Tiempo CPU Memoria" > "$log_file"
+fi
+
+# Se monitorea el proceso cuando se está ejecutando
+while is_brave_running; do
+  current_time=$(awk '{print int($1)}' /proc/uptime)
+  elapsed_time=$((current_time - start_time))
+  if [ "$elapsed_time" -le "$duracion_monitoreo" ]; then
+    ps -C "$ejecutable" -o %cpu,%mem | tail -n 1 >> "$log_file"
+    sleep 1
+  else
+    break
+  fi
+done
