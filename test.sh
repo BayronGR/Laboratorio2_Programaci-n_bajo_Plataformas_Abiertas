013bba256c51c07c7bbc18089c371b5d08be9611
Bayron Guerrero Ruiz <162919765+BayronGR@users.noreply.github.com>
2024-04-11 12:45:49 -0600

    Script para la obtención de información

--- test.sh
+++ test.sh
@@ -17,3 +17,11 @@ if [ -z "$info" ] || [ -z "$executable_path" ]; then
     echo "ID $process_id no encontrado"
     exit 1
 fi
+
+# Se obtiene la información relevante del proceso
+process_name=$(echo "$info" | awk '{print $1}')
+process_pid=$(echo "$info" | awk '{print $2}')
+user=$(echo "$info" | awk '{print $3}')
+cpu_usage=$(echo "$info" | awk '{print $4}')
+mem_usage=$(echo "$info" | awk '{print $5}')
+status=$(echo "$info" | awk '{print $6}')
